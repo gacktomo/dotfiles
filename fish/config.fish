a0dfa842ac6c6a2cd5486c560adaab357cb4dbd1
Tomohiro Matsuo <tomohmat@yahoo-corp.jp>
2020-05-14 11:24:08 +0900

    Fix git alias

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,8 @@ alias gs='git status'
 alias ga='git add -A'
 alias gl="git log --graph --oneline --decorate=full -20 --date=short --format='%C(yellow)%h%C(reset) %C(magenta)[%ad]%C(reset)%C(auto)%d%C(reset) %s %C(cyan)@%an%C(reset)'"
 alias gca='git add -A && git commit -m'
-alias gpull='git pull origin master'
-alias gpush='git push origin master'
+alias gpull='git pull origin HEAD'
+alias gpush='git push origin HEAD'
 alias gcopr='~/dotfiles/gcopr.sh'
 alias c='clear'
 set -U fish_user_paths  ~/.rbenv/bin /usr/local/bin/flutter/bin ~/.nodebrew/current/bin $fish_user_paths
